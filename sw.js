27935891cfd19cc6c8634edb1f416ce2f3a9b465
Alisson Salvador <85083466+Howami-AS@users.noreply.github.com>
2026-09-07 22:37:17 -0300

    atualização

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'pwa-vendas-v4';
-const APP_SHELL = ['./', './index.html', './manifest.json', './icon-192.svg', './icon-512.svg'];
+const APP_SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
   event.waitUntil(
